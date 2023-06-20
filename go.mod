fc8046ccf3c04a4df979d7f2e53d5b7c9fc154c7
Murilo Silva <murilosilvavmt@gmail.com>
2023-06-20 17:04:12 +0000

    newClient code

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module main
 
 go 1.20
-
-require github.com/onosproject/onos-ric-sdk-go v0.8.11 // indirect
--- src/e2tConnectivity.go
+++ src/e2tConnectivity.go
@@ -1,5 +1,21 @@
 package qmaiXapp
 
-func e2tConnect() {
+// import ONF ONOS RIC SDK
+import (
+	"fmt"
 
+	e2client "github.com/onosproject/onos-ric-sdk-go/pkg/e2/v1beta1"
+)
+
+// Defines service model name and version to be used for creating an  E2 client.
+const (
+	svcModelName    = "oran-e2sm-kpm"
+	svcModelVersion = "v2"
+)
+
+func NewE2Client() {
+	client := e2client.NewClient(e2client.WithE2TAddress("onos-e2t", 5150),
+		e2client.WithServiceModel(e2client.ServiceModelName(svcModelName), e2client.ServiceModelVersion(svcModelVersion)),
+		e2client.WithEncoding(e2client.ProtoEncoding))
+	fmt.Println(client)
 }
