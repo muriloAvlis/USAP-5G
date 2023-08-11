18dfe4b27c53f9e86b141d0506d429f8e4301946
Murilo Silva <murilosilvavmt@gmail.com>
2023-08-11 19:40:33 +0000

    oi yoshi

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/muriloAvlis/qmai/pkg/e2Connectivity"
+	e2 "github.com/onosproject/onos-ric-sdk-go/pkg/e2/v1beta1"
 )
 
 // Defines service model name and version to be used for creating an  E2 client.
@@ -14,5 +15,5 @@ const (
 
 func main() {
 	e2Client := e2Connectivity.NewE2Client(serviceModelName, serviceModelVersion)
-	fmt.Println(e2Client.Node())
+	fmt.Println(e2Client.Node(e2.NodeID("e2:4/e00/2/64")))
 }
