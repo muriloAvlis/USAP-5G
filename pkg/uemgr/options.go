027ae9b84533ebc38dca08bde3e9744825e194b3
Murilo Silva <murilosilvavmt@gmail.com>
2024-02-29 15:21:02 -0300

    add watchUEConnections func

--- pkg/uemgr/options.go
+++ pkg/uemgr/options.go
@@ -2,6 +2,9 @@ package uemgr
 
 import "github.com/onosproject/onos-api/go/onos/uenib"
 
+// default aspect types to get of UEs
+var defaultAspectTypes = []string{}
+
 type Manager struct {
 	ueClient uenib.UEServiceClient
 }
@@ -10,3 +13,10 @@ type Config struct {
 	UeNibEndpoint string
 	UeNibPort     int
 }
+
+type UE struct {
+	ID          string
+	aspectTypes []string
+}
+
+type UEs []UE
