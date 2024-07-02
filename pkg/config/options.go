991054b16a2467ad38b345ad20763117812f244f
Murilo Silva <murilosilvavmt@gmail.com>
2024-07-02 09:58:10 -0300

    restructure repository

--- pkg/config/options.go
+++
@@ -1,8 +0,0 @@
-package config
-
-const (
-	// report period config path in json file
-	ReportPeriodConfigPath = "/reportPeriod/interval"
-	// granularity period config path in json file
-	GranularityPeriodConfigPath = "/reportPeriod/granularity"
-)
