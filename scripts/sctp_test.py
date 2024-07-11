006390eff7b512c238c78b349e325f277d92d369
Murilo Silva <murilosilvavmt@gmail.com>
2024-07-11 14:11:54 -0300

    updates on SRSRAN docker image | fix on sctp_test script

--- scripts/sctp_test.py
+++ scripts/sctp_test.py
@@ -1,14 +1,23 @@
+# How to use: python3 sctp_test.py {IP} {PORT}
+
 import socket
+import sys
+
 
 def check_sctp_port(ip, port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_SEQPACKET, socket.IPPROTO_SCTP)
-        sock.settimeout(5)  # Timeout após 5 segundos
+        sock = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_SCTP)
+        sock.settimeout(1)  # Timeout after 1 second
         sock.connect((ip, port))
         print(f"SCTP port {port} on {ip} is open.")
         sock.close()
     except socket.error as e:
         print(f"Cannot connect to SCTP port {port} on {ip}: {e}")
 
-# Substitua pelo IP e porta desejados
-check_sctp_port('10.43.195.113', 36421)
+
+IP = sys.argv[1]
+SCTP_PORT = int(sys.argv[2])
+
+# Replace by args
+check_sctp_port(IP, SCTP_PORT)
