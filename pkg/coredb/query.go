63cc54608f067b5be9d26ae85f2099bfae5ec731
Murilo Silva <murilosilvavmt@gmail.com>
2024-07-17 23:13:18 -0300

    add filter by UE ID on AuthenticationSubscription table

--- pkg/coredb/query.go
+++ pkg/coredb/query.go
@@ -7,49 +7,89 @@ import (
 )
 
 // Get AuthenticationSubscription Table
-func (cdb *coreDB) getAuthenticationSubscription() ([]AuthenticationSubscriptionTable, error) {
-	var AuthSubs []AuthenticationSubscriptionTable // stores table rows
+func (cdb *coreDB) getAuthSubs() ([]AuthenticationSubscriptionTable, error) {
+	var authSubs []AuthenticationSubscriptionTable // stores table rows
 	rows, err := cdb.dbHdlr.Query("SELECT * FROM AuthenticationSubscription")
 	if err != nil {
 		return nil, fmt.Errorf("unable to get informations from authenticationSubscription table: %v", err)
 	}
 	defer rows.Close()
 
-	var SequenceNumber sql.NullString // stores SequenceNumber json format
+	var sequenceNumber sql.NullString // stores sequenceNumber json format
 
 	for rows.Next() {
-		var AuthSub AuthenticationSubscriptionTable // stores table rown
+		var authSub AuthenticationSubscriptionTable // stores table rown
 		err = rows.Scan(
-			&AuthSub.Ueid,
-			&AuthSub.AuthenticationMethod,
-			&AuthSub.EncPermanentKey,
-			&AuthSub.ProtectionParameterId,
-			&SequenceNumber,
-			&AuthSub.AuthenticationManagementField,
-			&AuthSub.AlgorithmId,
-			&AuthSub.EncOpcKey,
-			&AuthSub.EncTopcKey,
-			&AuthSub.VectorGenerationInHss,
-			&AuthSub.N5gcAuthMethod,
-			&AuthSub.RgAuthenticationInd,
-			&AuthSub.Supi,
+			&authSub.Ueid,
+			&authSub.AuthenticationMethod,
+			&authSub.EncPermanentKey,
+			&authSub.ProtectionParameterId,
+			&sequenceNumber,
+			&authSub.AuthenticationManagementField,
+			&authSub.AlgorithmId,
+			&authSub.EncOpcKey,
+			&authSub.EncTopcKey,
+			&authSub.VectorGenerationInHss,
+			&authSub.N5gcAuthMethod,
+			&authSub.RgAuthenticationInd,
+			&authSub.Supi,
 		)
 		if err != nil {
 			return nil, fmt.Errorf("unable to get informations from authenticationSubscription table: %v", err)
 		}
 
-		// Deserialize SequenceNumber data
-		err = json.Unmarshal([]byte(SequenceNumber.String), &AuthSub.SequenceNumber)
+		// Deserialize sequenceNumber data
+		err = json.Unmarshal([]byte(sequenceNumber.String), &authSub.SequenceNumber)
 		if err != nil {
 			return nil, fmt.Errorf("error to process sequenceNumber column: %v", err)
 		}
 
-		AuthSubs = append(AuthSubs, AuthSub) // add rown
+		authSubs = append(authSubs, authSub) // add rown
 	}
 
 	if err = rows.Err(); err != nil {
 		return nil, fmt.Errorf("unable to get informations from authenticationSubscription table: %v", err)
 	}
 
-	return AuthSubs, nil // return table rows
+	return authSubs, nil // return table rows
+}
+
+// Get AuthenticationSubscription Table by UE ID
+func (cdb *coreDB) getAuthSubsByUEId(UeId string) (AuthenticationSubscriptionTable, error) {
+	var authSub AuthenticationSubscriptionTable // store result
+	// get row from AuthenticationSubscription Table
+	row := cdb.dbHdlr.QueryRow("SELECT * FROM AuthenticationSubscription WHERE ueid = ?", UeId)
+
+	var sequenceNumber sql.NullString // stores sequenceNumber json format
+
+	err := row.Scan(
+		&authSub.Ueid,
+		&authSub.AuthenticationMethod,
+		&authSub.EncPermanentKey,
+		&authSub.ProtectionParameterId,
+		&sequenceNumber,
+		&authSub.AuthenticationManagementField,
+		&authSub.AlgorithmId,
+		&authSub.EncOpcKey,
+		&authSub.EncTopcKey,
+		&authSub.VectorGenerationInHss,
+		&authSub.N5gcAuthMethod,
+		&authSub.RgAuthenticationInd,
+		&authSub.Supi,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return authSub, fmt.Errorf("ue with id %s not found on authenticationSubscription table: %v", UeId, err)
+		}
+		return authSub, fmt.Errorf("unable to get ue informations from authenticationSubscription table: %v", err)
+	}
+
+	// Deserialize sequenceNumber data
+	err = json.Unmarshal([]byte(sequenceNumber.String), &authSub.SequenceNumber)
+	if err != nil {
+		return authSub, fmt.Errorf("error to process sequenceNumber column: %v", err)
+	}
+
+	return authSub, nil
 }
