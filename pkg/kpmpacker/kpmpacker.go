3f3e7c2e5a0d72880bc6fdfa1c5dc964d9453680
Murilo Silva <murilosilvavmt@gmail.com>
2024-08-12 16:15:18 -0300

    fix debug

--- pkg/kpmpacker/kpmpacker.go
+++ pkg/kpmpacker/kpmpacker.go
@@ -56,8 +56,8 @@ func EncodeActionDefinitionFormat4(metricNames []string, granularityPeriod uint6
 	// Convert array of pointers to char ** array
 	cMetricNamesPtr := (**C.uchar)(unsafe.Pointer(&cMetricNames[0]))
 
-	fmt.Printf("Go Metric Names: %v", metricNames)
-	fmt.Printf("Go Metric Names length: %v", len(metricNames))
+	fmt.Printf("Go Metric Names: %v\n", metricNames)
+	fmt.Printf("Go Metric Names length: %v\n", len(metricNames))
 	fmt.Printf("C Metric Names: %v\n", cMetricNames)
 	fmt.Printf("C Metric names length: %v\n", cNumOfMetrics)
 
