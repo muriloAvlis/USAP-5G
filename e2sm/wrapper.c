924e396961410a3f895b1c0a95e6894aec6881b7
Murilo Silva <murilosilvavmt@gmail.com>
2024-08-14 09:57:53 -0300

    test

--- e2sm/wrapper.c
+++ e2sm/wrapper.c
@@ -305,7 +305,7 @@ encodedData_t encodeActionDefinitionFormat4(char **metricNames, const size_t num
         return encoded;
     }
 
-    // xer_fprint(stdout, &asn_DEF_E2SM_KPM_ActionDefinition, actDef);
+    xer_fprint(stdout, &asn_DEF_E2SM_KPM_ActionDefinition, actDef);
 
     // Encoding
     asn_enc_rval_t enc_val = uper_encode_to_buffer(&asn_DEF_E2SM_KPM_ActionDefinition, NULL, actDef, buffer, buffer_size);
