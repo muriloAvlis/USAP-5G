33707464ccae917c177eca33eb6adbd1226a36f9
Murilo Silva <murilosilvavmt@gmail.com>
2024-08-19 19:39:08 -0300

    E2SM: split encoder/decoder logic

--- e2sm/kpm_encoder/src/wrapper.c
+++ e2sm/kpm_encoder/src/wrapper.c
@@ -4,144 +4,6 @@
 
 #include "wrapper.h"
 
-
-// TODO: fix empty metrics return when call this func
-actFmtType_t buildRanCellUeKpi(const char* ranFuncDefinition)
-{
-    actFmtType_t res = {};
-
-    // Calculate the length of the hex string
-    size_t rfDefLen = strlen(ranFuncDefinition);
-    printf("RFLenght: %ld\n", rfDefLen);
-    printf("RFLenghtAdapt: %ld\n", rfDefLen / 2 + 1);
-
-    // Allocate memory for a char array to store the hex values
-    char *rfDefBuffer = (char *)malloc((rfDefLen / 2) + 1); // Each byte is represented by 2 characters, +1 for null terminator
-    if (rfDefBuffer == NULL) {
-        fprintf(stderr, "rfDefBuffer memory allocation failed\n");
-        return res;
-    }
-
-    // Convert the hex string to binary data
-    for (size_t i = 0; i < rfDefLen; i += 2) {
-        char byte[3] = {ranFuncDefinition[i], ranFuncDefinition[i + 1], '\0'};
-        rfDefBuffer[i / 2] = (char)strtol(byte, NULL, 16);
-    }
-
-    // Null-terminate the char array
-    rfDefBuffer[rfDefLen / 2] = '\0';
-
-    // Now hex_buffer contains the binary data corresponding to the RF Definitions values
-    // Print the result
-    printf("[INFO] RAN Function definition values as a string: %s\n", rfDefBuffer);
-
-    char ** act_fmt_type1 = NULL;
-    char ** act_fmt_type2 = NULL;
-    char ** act_fmt_type3 = NULL;
-    char ** act_fmt_type4 = NULL;
-    char ** act_fmt_type5 = NULL;
-    int act_fmt_type1_size = 0;
-    int act_fmt_type2_size = 0;
-    int act_fmt_type3_size = 0;
-    int act_fmt_type4_size = 0;
-    int act_fmt_type5_size = 0;
-
-    E2SM_KPM_RANfunction_Description_t *e2smKpmRanFunctDescrip = (E2SM_KPM_RANfunction_Description_t *)(calloc(1, sizeof(E2SM_KPM_RANfunction_Description_t)));
-    if (e2smKpmRanFunctDescrip == NULL) {
-        fprintf(stderr, "e2smKpmRanFunctDescrip memory allocation failed\n");
-        return res;
-    }
-
-    // decode asn.1 format
-    const enum asn_transfer_syntax syntax = ATS_ALIGNED_BASIC_PER;
-    asn_dec_rval_t rval = asn_decode(NULL, syntax, &asn_DEF_E2SM_KPM_RANfunction_Description, (void**)&e2smKpmRanFunctDescrip, rfDefBuffer, rfDefLen);
-
-    if (rval.code == RC_OK)
-    {
-        printf( "[INFO] E2SM KPM RAN Function Description decode successfull rval.code = %d \n", rval.code);
-        // iterate over report styles | act_def
-        for (size_t i = 0; i < e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.count; i++)
-        {
-            switch (e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->ric_ActionFormat_Type)
-            {
-            case 1: // act_fmt_type_1
-                act_fmt_type1_size = e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.count;
-                act_fmt_type1 = (char **)malloc(act_fmt_type1_size * sizeof(char *));
-                for (size_t j = 0; j < act_fmt_type1_size; j++)
-                {
-                    size_t bufsize=e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.size;
-                    act_fmt_type1[j] = (char*)malloc(bufsize);
-                    act_fmt_type1[j] = (char*)e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.buf;
-                }
-                break;
-            case 2: // act_fmt_type_2
-                act_fmt_type2_size = e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.count;
-                act_fmt_type2 = (char **)malloc(act_fmt_type2_size * sizeof(char *));
-                for (size_t j = 0; j < act_fmt_type2_size; j++)
-                {
-                    size_t bufsize=e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.size;
-                    act_fmt_type2[j] = (char*)malloc(bufsize);
-                    act_fmt_type2[j] = (char*)e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.buf;
-                }
-                break;
-            case 3: // act_fmt_type_3
-                act_fmt_type3_size = e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.count;
-                act_fmt_type3 = (char **)malloc(act_fmt_type3_size * sizeof(char *));
-                for (size_t j = 0; j < act_fmt_type3_size; j++)
-                {
-                    size_t bufsize=e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.size;
-                    act_fmt_type3[j] = (char*)malloc(bufsize);
-                    act_fmt_type3[j] = (char*)e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.buf;
-                }
-                break;
-            case 4: // act_fmt_type_4
-                act_fmt_type4_size = e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.count;
-                act_fmt_type4 = (char **)malloc(act_fmt_type4_size * sizeof(char *));
-                for (size_t j = 0; j < act_fmt_type4_size; j++)
-                {
-                    size_t bufsize=e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.size;
-                    act_fmt_type4[j] = (char*)malloc(bufsize);
-                    act_fmt_type4[j] = (char*)e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.buf;
-                }
-                break;
-            case 5: // act_fmt_type_5
-                act_fmt_type5_size = e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.count;
-                act_fmt_type5 = (char **)malloc(act_fmt_type5_size * sizeof(char *));
-                for (size_t j = 0; j < act_fmt_type5_size; j++)
-                {
-                    size_t bufsize=e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.size;
-                    act_fmt_type5[j] = (char*)malloc(bufsize);
-                    act_fmt_type5[j] = (char*)e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->measInfo_Action_List.list.array[j]->measName.buf;
-                }
-                break;
-            default:
-                printf("[WARN] Unknown action definition format %ld!", e2smKpmRanFunctDescrip->ric_ReportStyle_List->list.array[i]->ric_ActionFormat_Type);
-                break;
-            }
-        }
-    } else
-    {
-        printf("[WARN] E2SM KPM RAN Function Description decode failed rval.code = %d \n", rval.code);
-    }
-
-    // set RAN Func definitions to res
-    res.act_fmt_type1 = act_fmt_type1;
-    res.act_fmt_type2 = act_fmt_type2;
-    res.act_fmt_type3 = act_fmt_type3;
-    res.act_fmt_type4 = act_fmt_type4;
-    res.act_fmt_type5 = act_fmt_type5;
-    res.act_fmt_type1_size = act_fmt_type1_size;
-    res.act_fmt_type2_size = act_fmt_type2_size;
-    res.act_fmt_type3_size = act_fmt_type3_size;
-    res.act_fmt_type4_size = act_fmt_type4_size;
-    res.act_fmt_type5_size = act_fmt_type5_size;
-
-    free(rfDefBuffer);
-    free(e2smKpmRanFunctDescrip);
-
-    return res;
-}
-
 encodedData_t encodeEventTriggerDefinitionFormat1(const u_int64_t reportingPeriod)
 {
     // Initialize the result
@@ -179,8 +41,6 @@ encodedData_t encodeEventTriggerDefinitionFormat1(const u_int64_t reportingPerio
     if(buffer == NULL) {
         fprintf(stderr, "[ERROR] Buffer memory allocation failure!\n");
         ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_EventTriggerDefinition, eventTriggerDef);
-        ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_EventTriggerDefinition_Format1, eventTriggerDefFmt1);
-        free(eventTriggerDefFmt1);
         return encoded;
     }
 
@@ -193,19 +53,21 @@ encodedData_t encodeEventTriggerDefinitionFormat1(const u_int64_t reportingPerio
     // Failed to encoding
     if (enc_rval.encoded == -1) {
         fprintf(stderr, "[ERROR] Failed to encode E2SM_KPM_EventTriggerDefinition!\n");
+        ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_EventTriggerDefinition, eventTriggerDef);
+        free(buffer);
         return encoded;
     }
 
-    // Adjust size (1 byte == 8 bits)
-    encoded.size = enc_rval.encoded / 8;
+    encoded.size = enc_rval.encoded;
     encoded.buffer = calloc(1, encoded.size);
-    for (size_t i = 0; i < encoded.size; i++) {
-        encoded.buffer[i] = buffer[i];
+    if (encoded.buffer == NULL) {
+        fprintf(stderr, "Memory allocation failure\n");
+        return encoded;
     }
 
+    memcpy(encoded.buffer, buffer, encoded.size);
+
     ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_EventTriggerDefinition, eventTriggerDef);
-    ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_EventTriggerDefinition_Format1, eventTriggerDefFmt1);
-    free(buffer); // free memory in the end
 
     return encoded;
 }
@@ -349,11 +211,6 @@ encodedData_t encodeActionDefinitionFormat4(char **metricNames, const size_t num
     }
 
     memcpy(encoded.buffer, buffer, encoded.size);
-
-    // free memory
-    ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_ActionDefinition, actDef);
-    ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_ActionDefinition_Format4, actDefFmt4);
-    free(buffer);
 
     return encoded;
 }
---
+++ pkg/kpmpacker/decoder.go
@@ -0,0 +1,56 @@
+package kpmpacker
+
+/*
+#cgo LDFLAGS: -lm -le2sm_kpm_decoder
+#cgo CFLAGS: -I /usr/local/include/e2sm/kpm_decoder
+#include <e2sm/kpm_decoder/wrapper.h>
+*/
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+// E2SM-KPM spec: 7.4.1 REPORT Service Style Type (1-5)
+func DecodeActFmtTypebyReportStyle(ranFunctionDefinition string, ricReportStyle int) []string {
+	RfDefCString := C.CString(ranFunctionDefinition)
+	defer C.free(unsafe.Pointer(RfDefCString))                  // free buffer
+	rfDefActFmtTypes := C.decodeActionFormatTypes(RfDefCString) // get RAN Function definitions from C wrapper
+
+	switch ricReportStyle {
+	case 1:
+		actionFormatType1 := make([]string, 0, rfDefActFmtTypes.act_fmt_type1_size)
+		for _, v := range unsafe.Slice(rfDefActFmtTypes.act_fmt_type1, rfDefActFmtTypes.act_fmt_type1_size) {
+			actionFormatType1 = append(actionFormatType1, C.GoString(v))
+		}
+		return actionFormatType1
+	case 2:
+		actionFormatType2 := make([]string, 0, rfDefActFmtTypes.act_fmt_type2_size)
+		for _, v := range unsafe.Slice(rfDefActFmtTypes.act_fmt_type2, rfDefActFmtTypes.act_fmt_type2_size) {
+			actionFormatType2 = append(actionFormatType2, C.GoString(v))
+		}
+		return actionFormatType2
+	case 3:
+		actionFormatType3 := make([]string, 0, rfDefActFmtTypes.act_fmt_type3_size)
+		for _, v := range unsafe.Slice(rfDefActFmtTypes.act_fmt_type3, rfDefActFmtTypes.act_fmt_type3_size) {
+			actionFormatType3 = append(actionFormatType3, C.GoString(v))
+		}
+		return actionFormatType3
+	case 4:
+		actionFormatType4 := make([]string, 0, rfDefActFmtTypes.act_fmt_type4_size)
+		for _, v := range unsafe.Slice(rfDefActFmtTypes.act_fmt_type4, rfDefActFmtTypes.act_fmt_type4_size) {
+			actionFormatType4 = append(actionFormatType4, C.GoString(v))
+		}
+		return actionFormatType4
+	case 5:
+		actionFormatType5 := make([]string, 0, rfDefActFmtTypes.act_fmt_type5_size)
+		for _, v := range unsafe.Slice(rfDefActFmtTypes.act_fmt_type5, rfDefActFmtTypes.act_fmt_type5_size) {
+			actionFormatType5 = append(actionFormatType5, C.GoString(v))
+		}
+		return actionFormatType5
+	default:
+		fmt.Println("[WARN] Unknown RAN Fuction Report Style!")
+	}
+
+	return nil
+}
