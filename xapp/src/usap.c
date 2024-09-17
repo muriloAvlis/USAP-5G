0f5dac7fbe9c14bb4f628d4f3aabf61342fd6a04
Murilo Silva <murilosilvavmt@gmail.com>
2024-09-17 20:33:42 -0300

    add C env development to xApp in FlexRIC

---
+++ xapp/src/usap.c
@@ -0,0 +1,5 @@
+//
+// Created by murilo on 17/09/24.
+//
+
+#include "xapp_usap.h"
---
+++ xapp/src/usap.h
@@ -0,0 +1,8 @@
+//
+// Created by murilo on 17/09/24.
+//
+
+#ifndef XAPP_USAP_H
+#define XAPP_USAP_H
+
+#endif //XAPP_USAP_H
