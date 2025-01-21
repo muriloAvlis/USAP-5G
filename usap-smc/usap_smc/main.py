dab0c055774fdc0970ec614c324f070bcf238361
Murilo Silva <murilosilvavmt@gmail.com>
2025-01-20 23:15:14 -0300

    add usap-smc Dockerfile | many updates

--- usap-smc/usap_smc/main.py
+++ usap-smc/usap_smc/main.py
@@ -1,1 +1,31 @@
-# TODO
+import sys
+import signal
+
+from usap_smc.logger.logger import Log
+
+logger = Log().get_logger()
+
+
+class App(object):
+    def __init__(self):
+        pass
+
+    def signal_handler(self, sig, frame):
+        """Handle graceful shutdown on SIGINT or SIGTERM."""
+        logger.info(
+            "Signal received: %s. Shutting down server gracefully.", sig)
+        sys.exit(0)
+
+    def Start(self):
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+        # TODO
+
+
+def run():
+    logger.info("Starting usap-e2ap application...")
+
+    # Call App class to start processes
+    app = App()
+    app.Start()
