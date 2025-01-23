0698d212063e55d92780adefc0cadd5a4deca7f6
Murilo Silva <murilosilvavmt@gmail.com>
2025-01-23 12:59:19 +0000

    LSTM model updated

--- usap-smc/usap_smc/ml_model/gen_lstm.py
+++ usap-smc/usap_smc/ml_model/gen_lstm.py
@@ -21,8 +21,8 @@ class GenModel(object):
 
         self.samples_per_block = 5
         self.max_epochs_tunner = 3000
-        self.epochs_model = 500
-        self.model_patience = 50
+        self.epochs_model = 1000
+        self.model_patience = 100
         self.factor = 3
 
         features = [
