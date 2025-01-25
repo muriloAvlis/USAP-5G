03fea1d2f6b0cbc4ccf6183bb22c04157eae6193
Murilo Silva <murilosilvavmt@gmail.com>
2025-01-25 18:44:08 -0300

    change e2sm logger

--- usap-e2sm/usap_e2sm/main.py
+++ usap-e2sm/usap_e2sm/main.py
@@ -5,17 +5,18 @@ from usap_e2sm.server.server import Server
 from usap_e2sm.logger.logger import Log
 
 
-logger = Log().get_logger()
+from loguru import logger
+from usap_smc.logger.logger import Log
+from usap_smc.client.client import Client
 
 
 class App():
     def __init__(self):
         self.grpc_server = Server()
-        self.logger = Log().get_logger()
 
     def signal_handler(self, sig, frame):
         """Handle graceful shutdown on SIGINT or SIGTERM."""
-        self.logger.info(
+        logger.info(
             "Signal received: %s. Shutting down server gracefully.", sig)
         sys.exit(0)
 
@@ -26,12 +27,14 @@ class App():
         try:
             self.grpc_server.Start()
         except Exception as e:
-            self.logger.error(
+            logger.error(
                 "Error while running the gRPC server: {}", str(e))
             sys.exit(1)
 
 
 def run():
+    Log().configure()
+
     logger.info("Starting usap-e2ap application...")
 
     app = App()
