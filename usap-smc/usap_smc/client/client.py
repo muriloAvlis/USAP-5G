e71ec366d8ab5a11dec52720aab541821ff05a48
Murilo Silva <murilosilvavmt@gmail.com>
2025-01-25 21:37:14 -0300

    test

--- usap-smc/usap_smc/client/client.py
+++ usap-smc/usap_smc/client/client.py
@@ -107,7 +107,7 @@ class Client(object):
                         ])
 
                         # Chama a função de inferência se o buffer estiver cheio
-                        if len(buffer[ue.imsi]) == 5:
+                        if len(buffer[ue.imsi]) == 1:
                             # Chama a função de inferência (TODO: dá pra fazer com multi thread ??)
                             inference_time_start = time.time()
                             sst_inference = self.model.get_sst_inference(
