bc5f5fbd9762c681fc774191ae63c42c387958ae
Murilo Silva <murilosilvavmt@gmail.com>
2025-07-14 14:45:59 -0300

    [fixes] srsran-zmq values-usap and entrypoint updated

--- charts/srsran-5g-zmq/resources/entrypoint.sh
+++ charts/srsran-5g-zmq/resources/entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 
 ip_check="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
 
-if [[ ! -z "$AMF_HOSTNAME" ]] ; then
+if [[ -n "$AMF_HOSTNAME" ]] ; then
     if [[ ${AMF_HOSTNAME} =~ ${ip_check} ]] ; then
         export AMF_ADDR=${AMF_HOSTNAME} ## is a IP format
     else
-        export AMF_ADDR="$(host -4 $AMF_HOSTNAME |awk '/has.*address/{print $NF; exit}')" ## is a hostname format
+        export AMF_ADDR="$(getent hosts $AMF_HOSTNAME | awk '{print $1; exit}')" ## is a hostname format
     fi
 fi
 
@@ -16,12 +16,11 @@ if [[ -z "${AMF_BIND_ADDR}" ]] ; then
     export AMF_BIND_ADDR="$(ip addr show $AMF_BIND_INTERFACE | grep -Po 'inet \K[\d.]+')"
 fi
 
-
 if [[ ! -z "$RIC_HOSTNAME" ]] ; then
     if [[ ${RIC_HOSTNAME} =~ ${ip_check} ]] ; then
         export RIC_ADDR=${RIC_HOSTNAME} ## is a IP format
     else
-        export RIC_ADDR="$(host -4 $RIC_HOSTNAME |awk '/has.*address/{print $NF; exit}')" ## is a hostname format
+        export RIC_ADDR="$(getent hosts $RIC_HOSTNAME | awk '{print $1; exit}')" ## is a hostname format
     fi
 fi
 
@@ -29,6 +28,9 @@ if [[ -z "${RIC_BIND_ADDR}" ]] ; then
     export RIC_BIND_ADDR="$(ip addr show $RIC_BIND_INTERFACE | grep -Po 'inet \K[\d.]+')"
 fi
 
-envsubst < /gnb-template.yml > /etc/config/gnb-config.yml
 
-stdbuf -o0 $RUN_AS -c /etc/config/gnb-config.yml
+envsubst < /gnb-template.yml > /etc/srsran/gnb-config.yml
+
+stdbuf -o0 $RUN_AS -c /etc/srsran/gnb-config.yml
+
+# while true; do sleep 10000; done
